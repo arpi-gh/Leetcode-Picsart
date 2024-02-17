22e5710c1a9a9eb786aa8fe60aa0a536a33d8dcd
arpi-gh <arpi.ghazaryan.ag@gmail.com>
2024-02-17 16:57:51 +0400

    Minor modifications to Two_Sum.

--- Twosum.py
+++ Twosum.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i, j]
+                    return i, j
 
     @staticmethod
     def two_sum_2(nums, target):
@@ -17,13 +17,13 @@ class Solution:
 
 
 if __name__ == '__main__':
-    inp = input()
+    inp = input('Enter a list of numbers: ')
     num_list = []
     for item in inp:
         if item.isdigit():
             num_list.append(int(item))
-    target_num = int(input())
-    # solution_1 = Solution().two_sum_1(num_list, target_num)
+    target_num = int(input('Enter the target number: '))
+    solution_1 = Solution().two_sum_1(num_list, target_num)
     solution_2 = Solution().two_sum_2(num_list, target_num)
-    # print('O(n^2) solution: ', solution_1)
+    print('O(n^2) solution: ', solution_1)
     print('O(n) solution: ', solution_2)

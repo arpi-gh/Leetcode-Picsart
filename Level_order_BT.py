43651c684d7bb859f175fd8c8e1827236dc4614c
arpi-gh <arpi.ghazaryan.ag@gmail.com>
2024-05-03 11:14:52 +0400

    Solved leetcode #102.

---
+++ Level_order_BT.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def __init__(self):
+        self.tree = []
+
+    def getHeight(self, node) -> int:
+        if not node:
+            return 0
+        left = self.getHeight(node.left)
+        right = self.getHeight(node.right)
+        return max(left, right) + 1
+
+    def dfs(self, node, height, cur_level):
+        if not node:
+            return
+        if cur_level < height:
+            if node.left:
+                self.tree[cur_level].append(node.left.val)
+            if node.right:
+                self.tree[cur_level].append(node.right.val)
+        self.dfs(node.left, height, cur_level=cur_level+1)
+        self.dfs(node.right, height, cur_level=cur_level+1)
+
+    def levelOrder(self, root: Optional[TreeNode]) -> list[list[int]]:
+        if root:
+            h = self.getHeight(root)
+            self.tree = [[] for _ in range(h)]
+            self.tree[0].append(root.val)
+            self.dfs(root, height=h, cur_level=1)
+        return self.tree
+
+
+node1 = TreeNode(3)
+node2 = TreeNode(9)
+node3 = TreeNode(20)
+node4 = TreeNode(15)
+node5 = TreeNode(7)
+
+node1.left = node2
+node1.right = node3
+node3.left = node4
+node3.right = node5
+
+print(Solution().levelOrder(node1))

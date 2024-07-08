e76cae6cb275d2220763c1cc5c983a2104320f83
arpi-gh <arpi.ghazaryan.ag@gmail.com>
2024-07-08 19:32:13 +0400

    Solved leetcode 1466.

--- Reorder_routes.py
+++ Reorder_routes.py
@@ -1,11 +1,12 @@
 class Solution(object):
+    def __init__(self):
+        self.reverse = 0
+
     def minReorder(self, n, connections):
         edges = {node: [] for node in range(n)}
         neighbors = {node: [] for node in range(n)}
-        visited = [False] * n
-        reverse = 0
-        order = []
         q = []
+        visited = [False] * n
         for s, d in connections:
             edges[s].append(d)
             neighbors[s].append(d)
@@ -14,27 +15,16 @@ class Solution(object):
         q.append(0)
         while q:
             current = q.pop(0)
-            order.append(current)
-            for n in neighbors[current]:
-                if n not in order:
-                    q.append(n)
-
-        def dfs(src):
-            nonlocal reverse
-            visited[src] = True
-            for neighbor in edges[src]:
-                if not visited[neighbor]:
-                    dfs(neighbor)
-                    reverse += 1
-
-        for node in order:
-            if not visited[node]:
-                dfs(node)
-
-        return reverse
+            visited[current] = True
+            for neigh in neighbors[current]:
+                if not visited[neigh]:
+                    q.append(neigh)
+                    if current not in edges[neigh]:
+                        self.reverse += 1
+        return self.reverse
 
 
 if __name__ == '__main__':
     sol = Solution()
-    c = [[1,2],[2,0]]
-    print(sol.minReorder(3, c))
+    c = [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]]
+    print(sol.minReorder(6, c))

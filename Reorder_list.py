699e94917aaa76c1f2f2fb5c3fabfe6c2e802646
arpi-gh <arpi.ghazaryan.ag@gmail.com>
2024-12-14 22:56:21 +0400

    Solved leetcode 143!

--- Reorder_list.py
+++ Reorder_list.py
@@ -8,6 +8,44 @@ class ListNode:
 
 
 # Time Limit Exceeded
+# class Solution:
+#     def reorderList(self, head: ListNode) -> None:
+#         if not head.next or not head.next.next:
+#             return
+#
+#         left = head
+#         right = head.next
+#         nodes = []
+#
+#         while right.next and right.next.next:
+#             left = left.next
+#             right = right.next.next
+#
+#         left = left.next
+#         while left:
+#             nodes.append(left)
+#             left = left.next
+#
+#         nodes.reverse()
+#         mid = len(nodes) - len(nodes) % 2
+#
+#         current = head
+#         for i in range(mid):
+#             print(f'{current} -> {current.next}')
+#             if nodes[i] == current.next:
+#                 break
+#             tmp = current.next
+#             current.next = nodes[i]
+#             print(f'{current} -> {current.next}')
+#             nodes[i].next = tmp
+#             current = tmp
+#
+#         if mid % 2 == 1:
+#             current.next = nodes[-1]
+#
+#         nodes[-1].next = None
+
+
 class Solution:
     def reorderList(self, head: ListNode) -> None:
         if not head.next or not head.next.next:
@@ -15,35 +53,45 @@ class Solution:
 
         left = head
         right = head.next
-        nodes = []
 
         while right.next and right.next.next:
             left = left.next
             right = right.next.next
 
-        left = left.next
-        while left:
-            nodes.append(left)
+        if right.next:
+            right = right.next
             left = left.next
 
-        nodes.reverse()
-        mid = len(nodes) - len(nodes) % 2
+        head_2 = left.next
+        left.next = None
 
-        current = head
-        for i in range(mid):
-            print(f'{current} -> {current.next}')
-            if nodes[i] == current.next:
-                break
-            tmp = current.next
-            current.next = nodes[i]
-            print(f'{current} -> {current.next}')
-            nodes[i].next = tmp
-            current = tmp
+        current = head_2
+        next_node = current.next
 
-        if mid % 2 == 1:
-            current.next = nodes[-1]
+        while next_node:
+            pivot = next_node
+            next_node = next_node.next
+            pivot.next = current
+            current = pivot
 
-        nodes[-1].next = None
+        head_2.next = None
+
+        left = head
+        right = current
+
+        while left and right:
+            l = left.next
+            r = right.next
+            left.next = right
+            right.next = l
+            left = l
+            last = right
+            right = r
+
+        if left:
+            last.next = left
+
+        return head
 
 
 if __name__ == '__main__':
@@ -51,10 +99,12 @@ if __name__ == '__main__':
     n2 = ListNode(2)
     n3 = ListNode(3)
     n4 = ListNode(4)
+    n5 = ListNode(5)
 
     n1.next = n2
     n2.next = n3
     n3.next = n4
+    n4.next = n5
 
     sol = Solution()
     sol.reorderList(n1)

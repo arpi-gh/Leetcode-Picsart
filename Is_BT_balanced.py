2d7781e6f0818b3107eb390c38327c265951d5d0
arpi-gh <arpi.ghazaryan.ag@gmail.com>
2024-12-22 16:20:59 +0400

    Found a more optimal solution for Leetcode 110!

--- Is_BT_balanced.py
+++ Is_BT_balanced.py
@@ -7,21 +7,44 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# old solution
+# class Solution:
+#     def getHeight(self, node) -> int:
+#         if not node:
+#             return 0
+#         left = self.getHeight(node.left)
+#         right = self.getHeight(node.right)
+#
+#         return max(left, right) + 1
+#
+#     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+#         if not root:
+#             return True
+#
+#         if not -1 <= self.getHeight(root.left) - self.getHeight(root.right) <= 1:
+#             return False
+#
+#         return self.isBalanced(root.left) and self.isBalanced(root.right)
 
+# new solution
 class Solution:
-    def getHeight(self, node) -> int:
-        if not node:
-            return 0
-        left = self.getHeight(node.left)
-        right = self.getHeight(node.right)
-
-        return max(left, right) + 1
+    def __init__(self):
+        self.res = True
 
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def getHeight(node):
+            if not node:
+                return 0
+            left = getHeight(node.left)
+            right = getHeight(node.right)
+
+            if abs(left - right) > 1:
+                self.res = False
+
+            return 1 + max(left, right)
+
         if not root:
             return True
 
-        if not -1 <= self.getHeight(root.left) - self.getHeight(root.right) <= 1:
-            return False
-
-        return self.isBalanced(root.left) and self.isBalanced(root.right)
+        getHeight(root)
+        return self.res

5e820a9e57a1bfbfc7568c749978879de5fae38f
arpi-gh <arpi.ghazaryan.ag@gmail.com>
2024-12-24 15:05:28 +0400

    Found a more optimal solution for Leetcode 112!

--- Has_path_sum_BT.py
+++ Has_path_sum_BT.py
@@ -6,54 +6,48 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# Old solution
 
-class Solution:
-    def __init__(self):
-        self.cur_sum = 0
-        self.found = False
-
-    def dfs(self, node, target_sum):
-        if not node:
-            return
-
-        self.cur_sum += node.val
-        if not node.left and not node.right:
-            if self.cur_sum == target_sum:
-                self.found = True
-                return
-
-        self.hasPathSum(node.left, target_sum)
-        self.hasPathSum(node.right, target_sum)
-
-        self.cur_sum -= node.val
-
-        return
-
-    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        self.dfs(root, targetSum)
-        return self.found
-
-# Doesn't work
 # class Solution:
 #     def __init__(self):
 #         self.cur_sum = 0
+#         self.found = False
 #
-#
-#     def hasPathSum(self, node: Optional[TreeNode], targetSum: int) -> bool:
+#     def dfs(self, node, target_sum):
 #         if not node:
 #             return
 #
 #         self.cur_sum += node.val
 #         if not node.left and not node.right:
-#             if self.cur_sum == targetSum:
-#                 return True
+#             if self.cur_sum == target_sum:
+#                 self.found = True
+#                 return
 #
-#         self.hasPathSum(node.left, targetSum)
-#         self.hasPathSum(node.right, targetSum)
+#         self.hasPathSum(node.left, target_sum)
+#         self.hasPathSum(node.right, target_sum)
 #
 #         self.cur_sum -= node.val
 #
-#         return False
+#         return
+#
+#     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+#         self.dfs(root, targetSum)
+#         return self.found
+
+# New solution
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int, curSum=0) -> bool:
+        if not root:
+            return False
+
+        curSum += root.val
+
+        if not root.left and not root.right:
+            return curSum == targetSum
+
+        return self.hasPathSum(root.left, targetSum, curSum) or self.hasPathSum(root.right, targetSum, curSum)
 
 
 node1 = TreeNode(1)
